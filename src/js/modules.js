82198930cfe3ca7c31f41cdc2673edb13a4340ea
iduk <iduk.design@gmail.com>
2019-01-29 12:37:15 +0900

    Commit.

--- src/js/modules.js
+++ src/js/modules.js
@@ -1,7 +1,8 @@
 // Pakage Modules JS
-
-// Select2
+// document ready
 $(function () {
+
+	// Select2
 	$('select').each(function () {
 		$(this).select2({
 			theme: 'bootstrap4',
@@ -10,23 +11,27 @@ $(function () {
 			allowClear: Boolean($(this).data('allow-clear')),
 		});
 	});
-});
 
 
-// wow.js 
-$(".wow").each(function () {
-	var wowHeight = $(this).height();
-	$(this).attr("data-wow-offset", wowHeight);
-});
 
-wow = new WOW({
-	boxClass: 'wow', // default
-	animateClass: 'animated', // default
-	offset: 0, // default
-	mobile: true, // default
-	live: true // default
+	// wow.js 
+	$(".wow").each(function () {
+		var wowHeight = $(this).height();
+		$(this).attr("data-wow-offset", wowHeight);
+	});
+
+	wow = new WOW({
+		boxClass: 'wow', // default
+		animateClass: 'animated', // default
+		offset: 0, // default
+		mobile: true, // default
+		live: true // default
+	});
+	wow.init();
+
+
 });
-wow.init();
+// document
 
 
 
